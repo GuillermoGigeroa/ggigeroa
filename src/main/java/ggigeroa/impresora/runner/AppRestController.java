bd024687e2f6e8df0849784aba2a8e7bd936b420
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2026-03-05 19:46:38 -0300

    Se corrige api

--- src/main/java/ggigeroa/impresora/runner/AppRestController.java
+++ src/main/java/ggigeroa/impresora/runner/AppRestController.java
@@ -1,50 +1,53 @@
 package ggigeroa.impresora.runner;
 
-import org.springframework.stereotype.Controller;
+import java.util.Map;
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Controller
+@RestController
 public class AppRestController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(AppRestController.class);
-	
-	@GetMapping("/")
-    public @ResponseBody Object getObject() {
+
+	@GetMapping(value = "/", produces = "application/json")
+	public Map<String, String> getObject() {
 		String message = "El sistema ha sido iniciado correctamente.";
 		logger.info(message);
-		return message;
-    }
-	
-	@GetMapping("/{path}/{id}")
-	public String getPath(@PathVariable (name = "path") String path, @PathVariable (name = "id") String id) {
-//		logger.info("GET called - path: {}, idUsuario: {}", path, id);
-//		StringBuilder sb = new StringBuilder();
-//		sb.append("Path: " + path);
-//		sb.append(" - ");
-//		sb.append("Id: " + id);
-		return "GET called - path: " + path + ", idUsuario: " + id;
+		Map<String, String> response = new HashMap<>();
+		response.put("status", "success");
+		response.put("message", message);
+		return response;
 	}
-	
-//	@PostMapping("/{path}/{id}")
-//	public String postPath(@PathVariable (name = "path") String path, @PathVariable (name = "id") String idUsuario, @RequestBody Object object) {
-//		logger.info("POST /{}/{} called - path: {}, idUsuario: {}", path, idUsuario, path, idUsuario);
-//		StringBuilder sb = new StringBuilder();
-//		sb.append("Path: " + path);
-//		sb.append(" - ");
-//		sb.append("Id: " + idUsuario);
-//		try {
-//			StringBuilder json = new StringBuilder((String) object);
-//			return sb.toString() + " - JSON: " + json.toString();
-//		} catch (Exception e) {
-//			logger.error("Error processing object", e);
-//		}
-//		return sb.toString();
-//	}
-	
+
+	@GetMapping(value = "/{path}/{id}", produces = "application/json")
+	public Map<String, Object> getPath(@PathVariable(name = "path") String path, @PathVariable(name = "id") String id) {
+		logger.info("GET called - path: {}, idUsuario: {}", path, id);
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", path);
+		response.put("idUsuario", id);
+		response.put("message", "GET called");
+		return response;
+	}
+
+	@PostMapping(value = "/{path}/{id}", consumes = "application/json", produces = "application/json")
+	public Map<String, Object> postPath(@PathVariable(name = "path") String path,
+			@PathVariable(name = "id") String idUsuario,
+			@RequestBody Map<String, Object> requestBody) {
+		logger.info("POST /{}/{} called - path: {}, idUsuario: {}", path, idUsuario, path, idUsuario);
+
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", path);
+		response.put("idUsuario", idUsuario);
+		response.put("message", "POST called");
+		response.put("receivedJson", requestBody);
+
+		return response;
+	}
+
 }
